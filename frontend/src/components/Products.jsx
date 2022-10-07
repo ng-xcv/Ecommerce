f434c7f822823b0fe90585174e7d2f7186b2c04e
ng-xcv <ngaryfaye95@gmail.com>
2022-10-07 15:09:48 +0000

    HTTPS over backend

--- frontend/src/components/Products.jsx
+++ frontend/src/components/Products.jsx
@@ -20,8 +20,8 @@ const Products = ({ cat, filters, sort }) => {
          try {
             const res = await axios.get(
                cat
-                  ? `http://localhost:5000/api/products?category=${cat}`
-                  : "http://localhost:5000/api/products"
+                  ? `https://44.210.140.91:6000/api/products?category=${cat}`
+                  : "https://44.210.140.91:6000/api/products"
             );
             setProducts(res.data);
          } catch (err) {}
